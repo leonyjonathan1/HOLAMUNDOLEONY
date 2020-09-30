7ad1b3b05bc27a271385c13145071d962c7d8600
Jonathan Leony <jonathanleony3000@gmail.com>
2020-09-29 22:38:07 -0600

    Agregue mi nombre despues de hola mundo

---
+++ ProgramaLeony/src/programaleony/ProgramaLeony.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programaleony;
+
+/**
+ *
+ * @author Jonathan Leony
+ */
+public class ProgramaLeony {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         System.out.println("Hola mundo");
+        System.out.println("Mi nombre es: Jonathan Renato Leony López CARNÉT: 2000048");
+        // TODO code application logic here
+    }
+    
+}
